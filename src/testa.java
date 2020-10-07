10ccb8595919bac5613fb3b812c458eeccf703fd
Panyanan286 <12597@DESKTOP-RP3U561>
2020-10-07 11:29:16 +0800

    aaa

--- src/testa.java
+++ src/testa.java
@@ -1,8 +1,40 @@
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class testa {
-	public static void main(){
+	public static void q(){
+		ArrayList<Integer> a =new ArrayList();
+		a.add(20);
+		a.add(50);
+		a.add(70);
+		a.add(60);
+		a.add(50);
+		a.add(50);
+		a.add(90);
+		a.add(80);
+		a.add(60);
+		a.add(50);
+		int max = a.get(0);
+		int min = a.get(0);
+		int i =0;
+		for(i=0;i<5;i++){
+			if(max < a.get(i))
+				max = a.get(i);
+			if(min > a.get(i))
+				min = a.get(i);
+			
+		}
+		System.out.println(min);
+		System.out.println(max);
 		
-		System.out.println("hello world 1");
+		System.out.println(a);
+		Collections.sort(a);
+		System.out.println(a);
+		
+		
+		
 	}
 
 }
